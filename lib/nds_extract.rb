ec53b6ad242b0e093a84dd44280ded4f44f9236e
Sean Welsh Brown <seanwelshbrown@gmail.com>
2019-12-02 15:13:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -30,6 +30,14 @@ end
 
 def gross_for_director(director_data)
   
+  total_for_director = 0
+  movie_list_length = director_data[:movies]
+  index = 0 
+  while index < movie_list_length do
+    total_for_director += director_data[:movies][index][:worldwide_gross]
+    index += 1 
+  end
   
+  total_for_director
   
 end
